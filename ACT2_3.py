3c6e2f6f6005c9e1cb989774061e4e57b8e47f50
Iker-Ns <2223_iker.rivera@iticbcn.cat>
2024-11-12 08:26:34 +0100

    Otro

---
+++ ACT2_3.py
@@ -0,0 +1,10 @@
+nota = 6.25
+
+if nota > 8:
+    print("L'alumne ha tret un excel·lent")
+elif nota >= 6.6:
+    print("L'alumne ha tret un notable")
+elif nota >= 5:
+    print("L'alumne ha aprovat")
+else:
+    print("L'alumne està suspès")
